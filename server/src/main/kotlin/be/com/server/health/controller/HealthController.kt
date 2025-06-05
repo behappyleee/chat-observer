ae23003624e7396ad7dfa45d8d4bd2388c687af2
behappyleee <72903623+behappyleee@users.noreply.github.com>
2025-06-05 21:42:06 +0900

    feat: add HealthController

---
+++ server/src/main/kotlin/be/com/server/health/controller/HealthController.kt
@@ -0,0 +1,14 @@
+package be.com.server.health.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    fun health(): String = HttpStatus.OK.name
+}
